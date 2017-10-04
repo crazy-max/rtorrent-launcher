3359a9a3c9d398fba07c2571ac9a24e4671018d9
crazy-max <webmaster@crazyws.fr>
2017-10-05 01:53:06 +0200

    Check if rtorrent is started on Travis

--- travis/run.sh
+++ travis/run.sh
@@ -2,4 +2,5 @@
 
 /etc/init.d/rtorrent start
 timeout 180 grep -q 'rtorrent started successfully' <(tail -f /var/log/rtorrent/rtorrent-launcher.log)
+timeout 180 grep -q 'worker_rtorrent: Starting thread.' <(tail -f /var/log/rtorrent/rtorrent.log)
 /etc/init.d/rtorrent stop
